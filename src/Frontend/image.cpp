c0814a4af2d76f3af4cebecba73785defe9585ea
Chengqian Che <cche@gs17964.sp.cs.cmu.edu>
2018-10-29 16:11:50 -0400

    starting working on matching

--- src/Frontend/image.cpp
+++ src/Frontend/image.cpp
@@ -1,18 +1,37 @@
-#include <image.h>
-
-Image::Image(){
+// "Copyright [2019] <Zimo Li, Chengqian Che>\"  [legal/copyright]",
+#include "image.h"
+#include <string>
+using namespace cv;
+Image::Image() {
     /*** TODO ***/
 }
 
-Image::Image(std::string fileName){
-    img = cv::imread(fileName);
+Image::Image(std::string fileName) {
+    img = imread(fileName);
 }
 
-void Image::downsampleImg(int rate){
-    cv::resize(img, img, img.size()/rate);
+void Image::downsampleImg(int rate) {
+    resize(img, img, img.size()/rate);
 }
 
-void Image::featureExtraction(const cv::Ptr<cv::FeatureDetector> detector){
+void Image::featureExtraction(const Ptr<FeatureDetector> detector) {
     detector->detect(img, kp);
     detector->compute(img, kp, desc);
+}
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+     std::cout <<" Usage: ./match inputImg.jpg" << std::endl;
+     return -1;
+    }
+
+    Mat image1, image2;
+    image1 = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+
+    namedWindow("Display Image", CV_WINDOW_AUTOSIZE);
+    imshow("Display Image", image1);
+
+    waitKey(0);
+    return 0;
+
 }
--- src/Frontend/image.h
+++ src/Frontend/image.h
@@ -1,35 +1,43 @@
-#include <opencv2/features2d.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/opencv.hpp>
+// "Copyright [2019] <Zimo Li, Chengqian Che>\"  [legal/copyright]",
+#ifndef IMAGE_H_
+#define IMAGE_H_
 #include <vector>
 #include <iostream>
 #include <map>
+#include <string>
+#include <opencv2/features2d.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
+class Image {
+ public:
+  cv::Mat img;
+  cv::Mat desc;
+  std::vector<cv::KeyPoint> kp;
 
-class Image{
-    public:
-        cv::Mat img;
-        cv::Mat desc;
-        std::vector<cv::KeyPoint> kp;
+  cv::Mat T;  // Pose matrix 4x4
+  cv::Mat P;  // Prokection matrix 3x4
 
-        cv::Mat T; // Pose matrix 4x4
-        cv::Mat P; // Prokection matrix 3x4
+  using kp_idx_t = size_t;
+  using landmark_idx_t = size_t;
+  using img_idx_t = size_t;
 
-        using kp_idx_t = size_t;
-        using landmark_idx_t = size_t;
-        using img_idx_t = size_t;
+  Image();
+  explicit Image(std::string fileName);
 
-        Image();
-        Image(std::string fileName);
+  void downsampleImg(int rate);
+  bool kp_match_exist(size_t kp_idx, size_t img_idx) {
+    return kp_matches[kp_idx].count(img_idx) > 0;
+    }
 
-        void downsampleImg(int rate);
-        bool kp_match_exist(size_t kp_idx, size_t img_idx) { return kp_matches[kp_idx].count(img_idx) > 0; };
-        bool kp_3d_exist(size_t kp_idx) { return kp_landmark.count(kp_idx) > 0; }
-        
-        void featureExtraction(const cv::Ptr<cv::FeatureDetector> detector);
+  bool kp_3d_exist(size_t kp_idx) { return kp_landmark.count(kp_idx) > 0; }
 
-    private:
-        std::map<kp_idx_t, std::map<img_idx_t, kp_idx_t>> kp_matches; // keypoint matches in other images
-        std::map<kp_idx_t, landmark_idx_t> kp_landmark; // keypoint to 3d points
+  void featureExtraction(const cv::Ptr<cv::FeatureDetector> detector);
 
+ private:
+  // keypoint matches in other images
+  std::map<kp_idx_t, std::map<img_idx_t, kp_idx_t>> kp_matches;
+  std::map<kp_idx_t, landmark_idx_t> kp_landmark;  // keypoint to 3d points
 };
+
+#endif  // IMAGE_H_
